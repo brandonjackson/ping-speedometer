171f0add8a5e3eae0491de00cd7007bac7f4665c
Brandon Jackson <brandonscottjackson@gmail.com>
2014-09-23 11:52:05 -0500

    ping.js: interval decreased to 0.1 seconds

--- ping.js
+++ ping.js
@@ -6,14 +6,16 @@ var sys = require('sys'),
 exports.fetch = function(target, options){
 
     options = _.extend({
-        repeats: 10
+        repeats: 10,
+        interval: 0.1
     }, options);
 
     var deferred = Q.defer();
 
-    console.log('Ping.fetch(): pinging '+target);
-
-    exec("ping -c " + options.repeats + " " + target, function(error, stdout, stderr) {
+    console.log('Ping.fetch(): pinging ' + target);
+    
+    var command = "ping -i " + options.interval + " -c " + options.repeats + " " + target;
+    exec(command, function(error, stdout, stderr) {
         if (error !== null) {
             console.log('Ping.fetch(): error.');
             deferred.reject(new Error(error));
